376c142fd4b7e58ba95fff74888c2bd2cfcce926
Santiago Bruno <santiagobruno870@gmail.com>
2023-08-28 16:38:33 -0300

    carga imagen

--- Program.py
+++ Program.py
@@ -1,19 +1,22 @@
-from io import BytesIO
 from tkinter import *
 from tkinter import ttk
 import tkinter as tk
 from PIL import Image, ImageTk
-import subprocess
-import PytonBeautifulSoup.Data as Data
 import requests
+from io import BytesIO
+import Data 
+
 
 # VARIABLES
+txt_url = Data.get_url()
+txt_Titulo = Data.get_TitlePage(txt_url)
+Txt_AnimeImagen = Data.get_AnimeImage(txt_url)
+Txt_AnimeTitulo = Data.get_AnimeTitle(txt_url)
+
+# ************************************ Create App ************************************************
 root = Tk()
 root.title = "Recomendaciones Personalizadas"
 root.geometry("1080x720")
-
-txt_url = Data.get_url()
-txt_Titulo = Data.get_TitlePage(txt_url)
 
 # Create A Main Frame
 main_frame = Frame(root)
@@ -43,21 +46,32 @@ Titulo.pack(pady=20)
 
 # ************************************ Imagen y Nombre del Anime ************************************************
 
-Txt_AnimeImagen = Data.get_AnimeImage(txt_url)
-Txt_AnimeTitulo = Data.get_AnimeTitle(txt_url)
 
-for imagen in Txt_AnimeImagen:
-
-    for DivsTitulos in Txt_AnimeTitulo:
-
-        # ********************** Imagen **********************
-        UrlImagen = Data.get_url()
-        UrlImagen = UrlImagen + imagen
-        lblImagen = tk.Label(Second_frame, text=UrlImagen, font=("Arial", 8))
-        lblImagen.pack(pady=20)
+count = 0
+image_labels = []
+for DivsTitulos, imagen in zip(Txt_AnimeTitulo, Txt_AnimeImagen) :
+    count = count + 1
+    if count > 2:
         # ********************** Nombre Del Anime **********************
         LblTexto = tk.Label(Second_frame, text=DivsTitulos, font=("Arial", 8))
         LblTexto.pack(pady=20)
+        
+        # ********************** Imagen **********************
+        #Descargar la imagen desde la URL
+        response = requests.get(imagen)
+        image_data = BytesIO(response.content)
+        image = Image.open(image_data)
+
+        #Convertir la imagen a un formato que Tkinter pueda manejar
+        tk_image = ImageTk.PhotoImage(image)
+
+        #Mostrar la imagen en un widget Label
+        image_label = tk.Label(Second_frame, image=tk_image)
+        image_label.pack()
+        
+        # Agregar la instancia de Label a la lista
+        image_labels.append(image_label)
+    
 
 
 root.mainloop()
